e8fdc19bca9fa87f4b53739dbd659ae1e507f17e
a22linre <a22linre@student.his.se>
2024-04-18 18:42:46 +0200

    implemented code for main activity and fixed compile errors

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.screens;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.buttonStartSecondActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("EXTRA_DATA", "Hello from MainActivity!");
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -0,0 +1,11 @@
+package com.example.screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+}
