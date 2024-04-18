e70e122baef2712a269ca0015fe5c7dd0915675e
a22linre <a22linre@student.his.se>
2024-04-18 18:43:18 +0200

    fixed the layouts and added code for the second activity

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -7,5 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
 
+        TextView textViewData = findViewById(R.id.textViewData);
+        Intent intent = getIntent();
+        if (intent.hasExtra("EXTRA_DATA")) {
+            String data = intent.getStringExtra("EXTRA_DATA");
+            textViewData.setText(data);
+        }
+    }
 }
